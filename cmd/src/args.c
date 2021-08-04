8114a0b03d0ec5b3b47ae1061e28cfd063a255e1
TornadoCookie <32080807+TornadoCookie@users.noreply.github.com>
2021-08-04 12:27:28 -0400

    the files didn't upload for some reason

---
+++ cmd/src/args.c
@@ -0,0 +1,103 @@
+
+#include <args.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+int ac;
+char **av;
+
+void setMainParams(int argc, char **argv)
+{
+	ac = argc;
+	av = argv;
+}
+
+char **argsToLookOutFor;
+int argCount;
+
+int *setArgsToLookOutFor(char **args, int count)
+{
+	int i;
+	int *res = malloc(count * sizeof(int)); 
+	for (i = 0; i < count; i++)
+	{
+		res[i] = i;
+		argsToLookOutFor[i] = args[i];
+	}
+	argCount = count;
+	return res;
+}
+
+bool checkIfUnrecognizedOptionIsPresent()
+{
+	int i, j;
+	bool unrecognized = true;
+	for (i = 1; i < ac; i++)
+	{
+		for (j = 0; j < argCount; j++)
+		{
+			if (strcmp(av[i], argsToLookOutFor[j]) == 0 || strcmp(av[i], strcat("-", argsToLookOutFor[j])) == 0)
+			{
+				unrecognized = false;
+			}
+		}
+		if (unrecognized == true)
+		{
+			break;
+		}
+	}
+	return unrecognized;
+}
+
+bool checkIfArgIsPresent(int arg)
+{
+	int i, j, k;
+	for (i = 1; i < ac; i++)
+	{
+			if (strlen(argsToLookOutFor[arg]) <= 2)
+			{
+				for (k = 0; k < strlen(av[i]); k++)
+				{
+					if (av[i][k] == argsToLookOutFor[arg][strlen(argsToLookOutFor[arg]) == 2 ? 1 : 0])
+					{
+						return true;
+					}
+				}
+			} 
+			else
+			{
+				if (strcmp(av[i], argsToLookOutFor[arg]) == 0)
+				{
+					return true;
+				}
+			}
+	}
+	return false;
+}
+
+int getIndexOfRecognizedArg(int arg)
+{
+	int i, j, k;
+        for (i = 1; i < ac; i++)
+        {
+                        if (strlen(argsToLookOutFor[arg]) <= 2)
+                        {
+                                for (k = 0; k < strlen(av[i]); k++)
+                                {
+                                        if (av[i][k] == argsToLookOutFor[arg][strlen(argsToLookOutFor[arg]) == 2 ? 1 : 0])
+                                        {
+                                                return i;
+                                        }
+                                }
+                        }
+                        else
+                        {
+                                if (strcmp(av[i], argsToLookOutFor[arg]) == 0)
+                                {
+                                        return i;
+                                }
+                        }
+        }
+        return 0;
+}
---
+++ cmd/src/cmd.c
@@ -0,0 +1,105 @@
+// 
+// KittenClan for command-line
+// Copyright (C) 2021 Helix Graziani
+//
+
+//cmd.c: Main.
+//This file puts everything together.
+//It contains the main function.
+
+//Include header files.
+#include <cats.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <defs.h>
+#include <args.h>
+
+#define arg_b       0x00
+#define arg_n       0x01
+#define arg_c       0x02
+#define arg_h       0x03
+#define arg_s       0x04
+#define arg_f       0x05
+#define arg_d       0x06
+#define arg_h       0x07
+#define arg_breed   0x08
+#define arg_name    0x09
+#define arg_cat     0x0A
+#define arg_help    0x0B
+#define arg_version 0x0C
+#define arg_verbose 0x0D
+#define arg_save    0x0E
+#define arg_file    0x0F
+#define arg_dob     0x10
+#define arg_dod     0x11
+#define arg_health  0x12
+
+//The current cat.
+//This could either be loaded from file,
+//or constructed on the spot.
+struct cat current_cat;
+
+//Function: pause
+
+//What it does:
+/*
+ * It prompts the user to press a key to continue.
+*/
+void pause(void)
+{
+	printf("\nPress any key to continue...\n");
+#ifdef _WIN32
+	getch();
+#else
+	getchar();
+#endif
+}
+
+//Function: main
+//You know what this function does.
+
+int main(int argc, char **argv)
+{
+    verbose = false;
+    bool noargs = argc == 1 ? true : false;
+    puts("\nKittenClan v1\n\n");
+    setMainParams(argc, argv);
+    char **argus = {
+	    [arg_b] = "-b",
+	    [arg_n] = "-n",
+	    [arg_c] = "-c",
+	    [arg_h] = "-h",
+	    [arg_s] = "-s",
+	    [arg_f] = "-f",
+	    [arg_d] = "-d",
+	    [arg_h] = "-h",
+	    [arg_breed] = "--breed",
+	    [arg_name] = "--name",
+	    [arg_cat] = "--cat",
+	    [arg_help] = "--help",
+	    [arg_version] = "--version",
+	    [arg_verbose] = "--verbose",
+	    [arg_save] = "--save",
+	    [arg_file] = "--file",
+	    [arg_dob] = "--dob",
+	    [arg_dod] = "--dod",
+	    [arg_health] = "--health"
+    };
+    int *args = setArgsToLookOutFor(argus);
+    if (checkIfUnrecognizedOptionIsPresent())
+    {
+	    puts("Unknown argument.\n");
+	    return 0;
+    }
+    if (checkIfArgIsPresent(arg_b) || checkIfArgIsPresent(arg_breed))
+    {
+	    
+    }
+    if (checkIfArgIsPresent(arg_n) || checkIfArgIsPresent(arg_name))
+    {
+	   current_cat.name =  
+    }
+    puts("\nAnd, above all else, remember this one fact:\nNot all cats are the same. Some require other needs, or might not fit the description in this application. That's OK.");
+    return 0;
+}
